3010cf8d23e9209840a61196173fa84c54482c89
mickelsonmichael <mickelsonmichael@gmail.com>
2020-04-26 12:02:39 -0600

    Add Karamja

--- src/domain/Diaries.cs
+++ src/domain/Diaries.cs
@@ -10,18 +10,21 @@ namespace Domain
         public Diary Falador { get; }
         public Diary Fremennik { get; }
         public Diary Desert { get; }
+        public Diary Karamja { get; }
 
         public Diaries(Diary kandarin,
             Diary ardougne,
             Diary falador,
             Diary fremennik,
-            Diary desert)
+            Diary desert,
+            Diary karamja)
         {
             Kandarin = kandarin;
             Ardougne = ardougne;
             Falador = falador;
             Fremennik = fremennik;
             Desert = desert;
+            Karamja = karamja;
         }
 
         public IEnumerable<(string Name, Diary Diary)> GetDiaries()
@@ -29,6 +32,7 @@ namespace Domain
             yield return (nameof(Ardougne), Ardougne);
             yield return (nameof(Falador), Falador);
             yield return (nameof(Fremennik), Fremennik);
+            yield return (nameof(Karamja), Karamja);
             yield return (nameof(Kandarin), Kandarin);
         }
     }
