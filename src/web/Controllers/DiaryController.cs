30060b774cae81575e5893a1066814cb0fa38f1f
mickelsonmichael <mickelsonmichael@gmail.com>
2020-04-30 08:24:09 -0600

    General UI Improvements

--- src/web/Controllers/DiaryController.cs
+++ src/web/Controllers/DiaryController.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using web.Models;
 using Services.Interfaces;
 
@@ -12,31 +7,18 @@ namespace web.Controllers
 {
     public class DiaryController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public DiaryController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [HttpPost]
-        public IActionResult GetDiaryRequirements(string playerName,
+        public IActionResult Index(string playerName,
             [FromServices] IAchievementService achievementService)
         {
             if (string.IsNullOrWhiteSpace(playerName))
             {
-                return View(nameof(Index));
+                return View("Index", "Home");
             }
 
-            var reqs = achievementService.GetDiaryProgress(playerName);
-
-            Console.WriteLine(reqs.Diaries);
-            return View("Requirements", reqs);
+            return View(
+                achievementService.GetDiaryProgress(playerName)
+            );
         }
     }
 }
