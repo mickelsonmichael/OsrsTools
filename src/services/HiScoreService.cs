72752c51c391c936201a8e477dd647bbbefc3a41
mickelsonmichael <mickelsonmichael@gmail.com>
2020-04-30 13:57:50 -0600

    Add basic caching

--- src/services/HiScoreService.cs
+++ src/services/HiScoreService.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Collections.Generic;
@@ -14,16 +16,77 @@ namespace Services
     {
         const string _apiUrl = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
         private WebClient _client = new WebClient();
-        
+        private IMemoryCache _cache;
+        private readonly ILogger<HiScoreService> _logger;
+        private const byte _retryAttempts = 3;
+
+        public HiScoreService(IMemoryCache cache, ILogger<HiScoreService> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+
         public HiScore GetHiScore(string playerName)
         {
-            var dataString = _client.DownloadString(_apiUrl + playerName);
-            System.Console.WriteLine(dataString);
+            _logger.LogInformation($"Getting HiScores for {playerName}");
+
+            return _cache.GetOrCreate(
+                playerName,
+                entry => 
+                {
+                    entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(60));
+                    entry.SetSlidingExpiration(TimeSpan.FromMinutes(15));
+
+                    return RetrieveFromApi(playerName);
+                }
+            );
+        }
+
+        private HiScore RetrieveFromApi(string playerName)
+        {
+            _logger.LogInformation($"Retrieving updated information for {playerName}");
+
+            var dataString = GetDataString(playerName);
+            
+            _logger.LogDebug(dataString);
 
             return new HiScore(
                 playerName, 
                 ParseHighScore(dataString)
             );
+        }
+
+        private string GetDataString(string playerName)
+        {
+            string dataString = string.Empty;
+            byte attemptCount = 1;
+
+            try 
+            {
+                while(true)
+                {
+                    dataString = _client.DownloadString(_apiUrl + playerName);
+
+                    if (string.IsNullOrEmpty(dataString))
+                    {
+                        throw new Exception("Empty string retrieved from database");
+                    }
+
+                    return dataString;
+                }
+            }
+            catch (Exception ex)
+                when (attemptCount < _retryAttempts)
+            {
+                _logger.LogError($"Failed attempt to gather data from HiScores. Attempt {attemptCount}/{_retryAttempts}", ex);
+                attemptCount++;
+            }
+            catch
+            {
+                throw; // retries failed, just throw
+            }
+
+            return string.Empty;
         }
 
         private IEnumerable<Skill> ParseHighScore(string data)
