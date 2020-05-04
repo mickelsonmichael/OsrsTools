67ce904e45f6d4e6900e0b4c10c05f73277e3c0b
mickelsonmichael <mickelsonmichael@gmail.com>
2020-05-04 15:47:11 -0600

    Add missing WebApp files

---
+++ src/web/WebApp/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/main.js'),
+    output: {
+        path: path.resolve(__dirname, '../wwwroot/js'),
+        filename: 'bundle.js'
+    },
+    mode: "development",
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({ "React": "react" })
+    ]
+};
