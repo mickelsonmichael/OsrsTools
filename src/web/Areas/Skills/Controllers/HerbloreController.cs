acd6b5a25b123599e71450cfe66d50dc109ee42d
mickelsonmichael <mickelsonmichael@gmail.com>
2020-05-04 09:07:14 -0600

    Add initial herblore page

---
+++ src/web/Areas/Skills/Controllers/HerbloreController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces.Skills;
+using Web.Utilities;
+
+namespace Web.Areas.Skills.Controllers
+{
+    [Area("Skills")]
+    public class HerbloreController : PlayerInfoController
+    {
+        private readonly IHerbloreService _herbloreService;
+
+        public HerbloreController(IHerbloreService herbloreService)
+        {
+            _herbloreService = herbloreService;
+        }
+
+        public IActionResult Index()
+            => View(_herbloreService.GetPotions());
+    }
+}
