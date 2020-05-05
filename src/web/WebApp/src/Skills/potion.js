a9f31879f82b246ea795f9824e2de96b67447dba
mickelsonmichael <mickelsonmichael@gmail.com>
2020-05-05 07:01:49 -0600

    Add grimy XP rows

--- src/web/WebApp/src/Skills/potion.js
+++ src/web/WebApp/src/Skills/potion.js
@@ -18,21 +18,39 @@ export default class Potion extends React.Component {
     }
 
     render() {
-        let totalXp = (this.props.clean + this.props.xp) * this.state.herbs;
+        let grimyXp = (this.props.clean + this.props.xp) * this.state.herbs;
+        let cleanXp = this.props.xp * this.state.herbs;
+        let rows = [
+            (
+                <tr key={"grimy"}>
+                    <td>{this.props.level}</td>
+                    <td>Grimy {this.props.name}</td>
+                    <td>{this.props.ingredients.join(",")}</td>
+                    <td>{this.props.herb}</td>
+                    <td>
+                        <input type="number" value={this.state.herbs} onChange={this.updateXp} min="0" />
+                    </td>
+                    <td>
+                        <NumberFormat value={cleanXp} displayType={"text"} thousandSeparator={true} decimalScale={1} allowNegative={false} />
+                    </td>
+                </tr>
+            ),
+            (
+                <tr key="clean">
+                    <td>{this.props.level}</td>
+                    <td>{this.props.name}</td>
+                    <td>{this.props.ingredients.join(",")}</td>
+                    <td>{this.props.herb}</td>
+                    <td>
+                        <input type="number" value={this.state.herbs} onChange={this.updateXp} min="0" />
+                    </td>
+                    <td>
+                        <NumberFormat value={grimyXp} displayType={"text"} thousandSeparator={true} decimalScale={1} allowNegative={false} />
+                    </td>
+                </tr>
+            )
+        ];
 
-        return (
-            <tr>
-                <td>{this.props.level}</td>
-                <td>{this.props.name}</td>
-                <td>{this.props.ingredients.join(",")}</td>
-                <td>{this.props.herb}</td>
-                <td>
-                    <input type="number" value={this.state.herbs} onChange={this.updateXp} min="0" />
-                </td>
-                <td>
-                    <NumberFormat value={totalXp} displayType={"text"} thousandSeparator={true} decimalScale={1} allowNegative={false} />
-                </td>
-            </tr>
-        );
+        return rows;
     }
 }
