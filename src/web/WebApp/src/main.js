1905e1e2a428b8b1f6b50c0783ab4152d4d52933
mickelsonmichael <mickelsonmichael@gmail.com>
2020-06-20 17:47:13 -0600

    Begin initial conversion to Redux

--- src/web/WebApp/src/main.js
+++ src/web/WebApp/src/main.js
@@ -1,1 +1,13 @@
-﻿import Herblore from './Skills/Herblore/herblore.js'
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
